5a6338ebd5843ccb3c673a79e2aaf615ed563033
nealholt <neal.holts@gmail.com>
2013-10-04 15:27:46 -0600

    I got rid of some profiling files that I felt were mere clutter.

--- profile.sh
+++ profile.sh
@@ -1,9 +1,9 @@
 
-python profiling/viewProfiling.py profiling/collisionHandling.profile > profiling/collisionHandling.txt
-python profiling/viewProfiling.py profiling/getObstacles.profile > profiling/getObstacles.txt
-python profiling/viewProfiling.py profiling/populateSpace.profile > profiling/populateSpace.txt
-python profiling/viewProfiling.py profiling/setClosestSprites.profile > profiling/setClosestSprites.txt
-python profiling/viewProfiling.py profiling/updateHudHelper.profile > profiling/updateHudHelper.txt
-python profiling/viewProfiling.py profiling/updateTangibles.profile > profiling/updateTangibles.txt
-python profiling/viewProfiling.py profiling/game.run.profile > profiling/game_run.txt #This is the most important one.
+#python profiling/viewProfiling.py profiling/collisionHandling.profile > profiling/collisionHandling.txt
+#python profiling/viewProfiling.py profiling/getObstacles.profile > profiling/getObstacles.txt
+#python profiling/viewProfiling.py profiling/populateSpace.profile > profiling/populateSpace.txt
+#python profiling/viewProfiling.py profiling/setClosestSprites.profile > profiling/setClosestSprites.txt
+#python profiling/viewProfiling.py profiling/updateHudHelper.profile > profiling/updateHudHelper.txt
+#python profiling/viewProfiling.py profiling/updateTangibles.profile > profiling/updateTangibles.txt
+python profiling/viewProfiling.py profiling/game.run.profile > profiling/game_run.txt
 
