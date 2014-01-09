c013a9b487ab33d7d2edee861da961de19efc709
nealholt <neal.holts@gmail.com>
2014-01-09 16:41:51 -0700

    created a profiling object.

---
+++ code/profilingObject.py
@@ -0,0 +1,10 @@
+import globalvars
+
+class ProfilingObject(physicalObject.PhysicalObject):
+	''' '''
+	def __init__(self):
+		physicalObject.PhysicalObject.__init__(self, 0, 0)
+
+	def update(self):
+		pass
+
